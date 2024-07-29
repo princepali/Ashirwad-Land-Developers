87cd811e723d482383efe8c0aa45ff1bf8c0fc31
AzureAD\PrincePali <prince.pali@bettercommerce.io>
2024-07-29 10:27:04 +0530

    Add project files.

---
+++ Ashirwad Land Developers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ashirwad_Land_Developers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
